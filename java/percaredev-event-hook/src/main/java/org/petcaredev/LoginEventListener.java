7a2e5575e91b0a4990e693906ef8e64fdb4d3208
malithie <malithim@wso2.com>
2024-03-15 13:38:55 +0530

    Add service to listen to login events from asgardeo.

---
+++ java/percaredev-event-hook/src/main/java/org/petcaredev/LoginEventListener.java
@@ -0,0 +1,15 @@
+package org.petcaredev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // implement an HTTP server that can listen to login events published from a websub hub and out put the event to the console.
+        
+        System.out.println( "Hello World!" );
+    }
+}
